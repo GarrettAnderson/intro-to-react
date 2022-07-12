a88f3452183b58494c26878e4d3e3b7d6af1db66
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-07-12 12:04:39 -0400

    added an if statment to add a condition to show a different message when username is a specific name otehrwise show the regular message. added to the welcome message component

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class WelcomeMessage extends Component {
     render() {
-        return (
-            <h3>
-                Welcome, {this.props.username}!
-            </h3>
+        console.log(this.props)
+        if (this.props.username === 'Flash') {
+            return <h2>OMG! It's the {this.props.username}</h2>
+        } else {
+            return <h3>Welcome, {this.props.username}</h3>
+        }
         );
     }
 }
